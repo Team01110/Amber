aa5accac4e63fd667668f5ded3d8970d0f1dc853
Derix10 <sejitbekovarsen12.@gmail.com>
2023-04-07 12:12:07 +0600

    Add Navigation and fragments

--- app/src/main/java/com/example/amber/fragment/firebase/RegistFragment.kt
+++ app/src/main/java/com/example/amber/fragment/firebase/RegistFragment.kt
@@ -1,10 +1,6 @@
 package com.example.amber.fragment.firebase
 
-import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 
 
 class RegistFragment : Fragment() {
--- app/src/main/java/com/example/amber/fragment/news/NewsFragment.kt
+++ app/src/main/java/com/example/amber/fragment/news/NewsFragment.kt
@@ -1,10 +1,6 @@
 package com.example.amber.fragment.news
 
-import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 
 
 class NewsFragment : Fragment() {
