a74ccd9194d8d8cf0b9cdc21c027f8ed98d456fb
sapararzyev <arzyev680@gmail.com>
2023-06-12 19:55:53 +0600

    add

--- app/src/main/java/com/example/amber/fragment/home/HomeFragment.kt
+++ app/src/main/java/com/example/amber/fragment/home/HomeFragment.kt
@@ -1,6 +1,7 @@
 package com.example.amber.fragment.home
 
 import android.util.Log
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import com.example.amber.base.BaseFragment
@@ -21,11 +22,15 @@ class HomeFragment : BaseFragment<FragmentHomeBinding, Any?>(FragmentHomeBinding
                 when (it) {
                     is UiState.Error -> {
                         Log.e("ololo", "setupRequest:{${it.msg}}")
+                        binding.notesBar.isVisible = false
                     }
 
-                    is UiState.Loading -> {}
+                    is UiState.Loading -> {
+                        binding.notesBar.isVisible = true
+                    }
 
                     is UiState.Success -> {
+                        binding.notesBar.isVisible = false
                         binding.rvHome.adapter = adapterRv
                         binding.rvHome2.adapter = adapterVp
                         adapterRv.submitList(it.data)
