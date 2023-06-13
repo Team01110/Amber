4088fef5daa9e3a5090fdfe0f11cc0febdb08853
sapararzyev <arzyev680@gmail.com>
2023-06-13 12:47:35 +0600

    add

--- app/src/main/java/com/example/amber/fragment/home/HomeFragment.kt
+++ app/src/main/java/com/example/amber/fragment/home/HomeFragment.kt
@@ -1,38 +1,75 @@
 package com.example.amber.fragment.home
 
+import android.os.Bundle
 import android.util.Log
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import com.example.amber.R
 import com.example.amber.base.BaseFragment
 import com.example.amber.databinding.FragmentHomeBinding
 import com.example.amber.fragment.utils.UiState
+import com.example.domain.model.Product
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
-class HomeFragment : BaseFragment<FragmentHomeBinding, Any?>(FragmentHomeBinding::inflate) {
+class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
     private val vm: HomeViewModel by viewModels()
-    private val adapterVp: HomeAdapterRv by lazy { HomeAdapterRv() }
-    private val adapterRv: HomeAdapterVp by lazy { HomeAdapterVp() }
+    private val adapterRv: HomeAdapterRv by lazy { HomeAdapterRv(this::onClickProduct) }
+    private val adapterVp: HomeAdapterVp by lazy { HomeAdapterVp() }
+
+    private fun onClickProduct(product: Product) {
+        val bundle = Bundle()
+        bundle.putSerializable(KEY_PRODUCT, product)
+        controller.navigate(R.id.action_homeFragment_to_productFragment, bundle)
+    }
 
     override fun setupRequest() {
         lifecycleScope.launch {
-            vm.state.collect {
+            vm.stateProduct.collect {
                 when (it) {
                     is UiState.Error -> {
                         Log.e("ololo", "setupRequest:{${it.msg}}")
+                        binding.notesBar.isVisible = false
                     }
 
-                    is UiState.Loading -> {}
+                    is UiState.Loading -> {
+                        binding.notesBar.isVisible = true
+                    }
 
                     is UiState.Success -> {
-                        binding.rvHome.adapter = adapterRv
-                        binding.rvHome2.adapter = adapterVp
+                        binding.notesBar.isVisible = false
+                        binding.rvHome2.adapter = adapterRv
                         adapterRv.submitList(it.data)
-                        adapterVp.submitList(it.data)
                     }
+                    else -> {}
                 }
             }
         }
+        lifecycleScope.launch {
+            vm.stateCategory.collect {
+                when (it) {
+                    is UiState.Error -> {
+                        binding.notesBar.isVisible = false
+                        Log.e("ololo", "setupRequest:{${it.msg}}")
+                    }
+
+                    is UiState.Loading -> { binding.notesBar.isVisible = true}
+
+                    is UiState.Success -> {
+                        binding.notesBar.isVisible = false
+                        binding.rvHome.adapter = adapterVp
+                        adapterVp.submitList(it.data)
+                    }
+                    else -> {}
+                }
+            }
+        }
+
+    }
+    companion object{
+        const val KEY_PRODUCT = "product"
+
     }
 }
